96f5eceeb07408a8cd9cbbb94977b5602bbebb17
Hector Polanco <hdp2112@columbia.edu>
2019-10-25 19:43:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,8 +15,7 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  result = load_library(file_path)[:get_emoticon][emoticon]
-  result ? result : 
+  load_library(file_path)[:get_emoticon][emoticon]
 end
 
 def get_english_meaning
